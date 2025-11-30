a70cf5b6ce3a31a0ad19ebc7afea17932692c0d9
TrippleSeven777 <133012114+killerboyyy777@users.noreply.github.com>
2025-11-30 16:29:50 +0100

    Rename plugin to TradeManagerPlus and add bot command
    Updated naming conventions from 'trademanagerplus' to 'TradeManagerPlus' across project files for consistency. Added IBotCommand2 implementation with a sample 'TEST' command to the plugin class.

--- trademanagerplus/trademanagerplus.cs
+++ trademanagerplus/trademanagerplus.cs
@@ -2,21 +2,29 @@ using System;
 using System.Threading.Tasks;
 using ArchiSteamFarm.Core;
 using ArchiSteamFarm.Plugins.Interfaces;
+using ArchiSteamFarm.Steam;
 using JetBrains.Annotations;
 
-namespace trademanagerplus;
+namespace TradeManagerPlus;
 
 #pragma warning disable CA1812 // ASF uses this class during runtime
 [UsedImplicitly]
-internal sealed class trademanagerplus : IGitHubPluginUpdates {
-	public string Name => nameof(trademanagerplus);
-	public string RepositoryName => "killerboyyy777/trademanagerplus";
-	public Version Version => typeof(trademanagerplus).Assembly.GetName().Version ?? throw new InvalidOperationException(nameof(Version));
+internal sealed class TradeManagerPlus : IGitHubPluginUpdates, IBotCommand2 {
+	public string Name => nameof(TradeManagerPlus);
+	public string RepositoryName => "killerboyyy777/TradeManagerPlus";
+	public Version Version => typeof(TradeManagerPlus).Assembly.GetName().Version ?? throw new InvalidOperationException(nameof(Version));
 
 	public Task OnLoaded() {
 		ASF.ArchiLogger.LogGenericInfo($"Hello {Name}!");
 
 		return Task.CompletedTask;
+	}
+
+	public Task<string?> OnBotCommand(Bot bot, EAccess access, string message, string[] args, ulong steamID = 0) {
+		return Task.FromResult(args[0].ToUpperInvariant() switch {
+			"TEST" => "Test command executed!",
+			_ => null
+		});
 	}
 }
 #pragma warning restore CA1812 // ASF uses this class during runtime
